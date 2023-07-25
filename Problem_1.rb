79483de0f599735caf3cbaa35e8614876d3a526f
Zuha <zuhaarana@gmail.com>
2023-07-25 13:03:52 +0500

    Problem 1

---
+++ Problem_1.rb
@@ -0,0 +1,15 @@
+class Calculator
+  def add(a, b)
+    @a = a
+    @b = b
+    result = @a+@b
+    return result
+  end
+
+  def subtract(a, b)
+   @a = a
+   @b = b
+    result = @a-@b
+    return result
+  end
+end
