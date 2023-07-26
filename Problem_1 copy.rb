3318d54ad4fd63c15db717cd66dbaedc783dfb30
Zuha <zuhaarana@gmail.com>
2023-07-26 10:14:20 +0500

    Problem 2

--- Problem_1 copy.rb
+++ Problem_1 copy.rb
@@ -1,24 +1,3 @@
-
-# Problem 1
-
-# Problem Statement
-# Create a class Calculator, which performs addition and subtraction of two numbers
-# at a time.
-
-# Solution:
-
-class Calculator
-  def add(num1, num2)
-    @num1 = num1
-    @num2 = num2
-    result = @num1 + @num2
-    return result
-  end
-
-  def subtract(a, b)
-    @num1 = num1
-    @num2 = num2
-    result = @num1 - @num2
-    return result
-  end
+def length_finder(input_array)
+    input_array.map {|i| i.length}
 end
