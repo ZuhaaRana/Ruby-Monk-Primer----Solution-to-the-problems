bada9f115d8cf45a81b4dcc167d99a07af340455
Zuha <zuhaarana@gmail.com>
2023-07-27 10:10:00 +0500

    Problem 3

--- Problem_3.rb
+++ Problem_3.rb
@@ -1,16 +1,11 @@
 
-# Problem 2
+# Problem 3
 
 # Problem Statement
-# Given an array containing some strings, return an array containing the length of those strings.
+# Given a sentence containing multiple words, find the frequency of a given word
+# in that sentence.
+# Construct a method named 'find_frequency' which accepts two arguments 
+# 'sentence' and 'word', both of which are String objects.
 
-# You are supposed to write a method named 'length_finder' to accomplish this task.
-
-# Example:
-# Given ['Ruby','Rails','C42'] the method should return [4,5,3]
-
-# Solution:
-
-def length_finder(input_array)
-    input_array.map {|i| i.length}
-end
+# Example: The method, given 'Ruby is The best language in the World' and 'the',
+# should return 2 (comparison should be case-insensitive).
