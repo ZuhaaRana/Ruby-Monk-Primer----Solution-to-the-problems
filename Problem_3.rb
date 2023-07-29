ac9d1f3e5821e3209111f7c6d02d6f48efe8e0fc
Zuha <zuhaarana@gmail.com>
2023-07-29 17:19:52 +0500

    Problem 3

--- Problem_3.rb
+++ Problem_3.rb
@@ -1,7 +1,8 @@
 
 # Problem 3
 
-# Problem Statement
+# Problem Statement:
+
 # Given a sentence containing multiple words, find the frequency of a given word
 # in that sentence.
 # Construct a method named 'find_frequency' which accepts two arguments 
@@ -9,6 +10,8 @@
 
 # Example: The method, given 'Ruby is The best language in the World' and 'the',
 # should return 2 (comparison should be case-insensitive).
+
+# Solution:
 
 def find_frequency(sentence, word)
     sentence.split.map { |x| x.downcase }.count(word.downcase)
