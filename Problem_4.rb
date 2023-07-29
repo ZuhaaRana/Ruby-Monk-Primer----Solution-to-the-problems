e19474140fe04f6c6d324e904e6a1164b8648bff
Zuha <zuhaarana@gmail.com>
2023-07-29 14:43:33 +0500

    Problem 4

--- Problem_4.rb
+++ Problem_4.rb
@@ -11,3 +11,4 @@
 # Given an array [1, 2, 3, 4, 5] and 2 should return two random numbers
 # from the given array. (Note: those two random numbers may be the same number.)
 # The method should return those random values in a new array.
+
