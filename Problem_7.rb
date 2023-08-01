10c5da1fdb9211d1c8903e520c91dfbe8b929a46
Zuha <zuhaarana@gmail.com>
2023-08-01 10:52:18 +0500

    Problem 7

--- Problem_7.rb
+++ Problem_7.rb
@@ -7,3 +7,11 @@
 # You are supposed to write a method palindrome? to accomplish this task.
 # Note Ignore whitespace and cases of characters.
 
+def palindrome?(sentence)
+  sentence1 = sentence.downcase
+  sentence2 = sentence1.gsub(/\W/,"")  
+  reverse_sentence = sentence2.reverse
+  reverse_sentence == sentence2
+end
+
+puts palindrome?("Never odd or even")
