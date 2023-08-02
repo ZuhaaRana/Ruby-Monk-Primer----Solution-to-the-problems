fbf1dc17d981ce1b86dbe282a84fe43fc7ca42a2
Zuha <zuhaarana@gmail.com>
2023-08-02 10:40:30 +0500

    Problem 8

--- Problem_8.rb
+++ Problem_8.rb
@@ -1,6 +1,9 @@
-# Problem Statement
-# Compute the sum of cubes for a given range a through b.
 
+# Problem 8
+
+# Problem Statement:
+# Compute the sum of cubes for a given range a through b.
 # Write a method called sum_of_cubes to accomplish this task
 
-# Example Given range 1 to 3 the method should return 36
+# Example:
+# Given range 1 to 3 the method should return 36
