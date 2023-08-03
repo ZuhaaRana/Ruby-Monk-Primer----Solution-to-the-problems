1f5cae8f43e876b9cdaf233c3d385795a78dfd5e
Zuha <zuhaarana@gmail.com>
2023-08-03 11:36:34 +0500

    Problem 8

--- Problem_9.rb
+++ Problem_9.rb
@@ -1,3 +1,15 @@
+
+# Problem 8
+
+# Problem Statement:
+# Given an Array, return the elements that are present exactly once in the array.
+# You need to write a method called non_duplicated_values to accomplish this task.
+
+# Example:
+# Given [1,2,2,3,3,4,5], the method should return [1,4,5]
+
+#Solution:
+
 def non_duplicated_values(values)
   values.keep_if { |eachValue| values.count(eachValue) == 1 }
 end
