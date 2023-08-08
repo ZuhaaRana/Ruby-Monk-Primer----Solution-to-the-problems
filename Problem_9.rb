6285566fc5bdab640ceea68c356e0d6b2b28bd3c
Zuha <zuhaarana@gmail.com>
2023-08-08 21:49:27 +0500

    Problem 9

--- Problem_9.rb
+++ Problem_9.rb
@@ -8,8 +8,7 @@
 # Example:
 # Given [1,2,2,3,3,4,5], the method should return [1,4,5]
 
-#Solution:
-
+# Solution:
 def non_duplicated_values(values)
   values.keep_if { |eachValue| values.count(eachValue) == 1 }
 end
