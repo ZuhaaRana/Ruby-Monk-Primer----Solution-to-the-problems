7adf66187a46d1eca1aa7b56ab5cf5c33a888f7e
Zuha <zuhaarana@gmail.com>
2023-08-08 21:51:35 +0500

    Problem 10

---
+++ Problem_10.rb
@@ -0,0 +1,19 @@
+# Problem Statement
+
+# Given a custom class MyArray,
+# write a 'sum' method that takes a block parameter.
+
+class MyArray
+  attr_reader :array
+
+  def initialize(array)
+    @array = array
+  end
+
+  def sum(initial_value = 0)
+    return array.inject(:+) + initial_value unless block_given?
+    sum = initial_value
+    array.each {|n| sum += yield(n) }
+    sum
+  end
+end
