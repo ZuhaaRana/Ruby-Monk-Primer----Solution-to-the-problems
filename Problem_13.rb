4945150a624bfcb70d4f4bb5638d0800b0f26a54
Zuha <zuhaarana@gmail.com>
2023-08-11 10:10:17 +0500

    Problem 13

--- Problem_13.rb
+++ Problem_13.rb
@@ -6,3 +6,9 @@
 # You are given some code in the form of lambdas.
 # Measure and return the time taken to execute that code.
 # You may use Time.now to get the current time.
+
+def exec_time(proc)
+  start_time = Time.now
+  proc.call
+  Time.now - start_time
+end
