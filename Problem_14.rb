7857310419afd2416c286389f8d5bcf662af93fa
Zuha <zuhaarana@gmail.com>
2023-08-11 10:18:23 +0500

    Problem 14

--- Problem_14.rb
+++ Problem_14.rb
@@ -1,2 +1,11 @@
 
-# Problem 14
+# Problem 14 Number shuffle
+
+# Problem Statement:
+# Given a 3 or 4 digit number with distinct digits, return a sorted array of 
+# all the unique numbers that can be formed with those digits.
+
+# Example:
+# Given: 123
+# Return: [123, 132, 213, 231, 312, 321]
+
