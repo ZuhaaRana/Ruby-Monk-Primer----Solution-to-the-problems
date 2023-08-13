a04dc49fc7168d8a26d184b3455521ab74ca39ec
Zuha <zuhaarana@gmail.com>
2023-08-13 14:04:52 +0500

    Problem 15

---
+++ Problem_15.rb
@@ -0,0 +1,12 @@
+
+# Problem 15
+
+# Problem Statement:
+# A restaurant has incoming orders for which you need to compute the costs based on the menu.
+# You might get multiple orders at times.
+
+# Examples:
+# if the order is {:rice => 1, :noodles => 1}, and the menu is {:rice => 3, :noodles => 2}, 
+# then the cost is 5.
+# If the orders are {:rice => 1, :noodles => 1} and {:rice => 2, :noodles => 2} and the menu
+# is {:rice => 3, :noodles => 2}, then the cost is 15.
