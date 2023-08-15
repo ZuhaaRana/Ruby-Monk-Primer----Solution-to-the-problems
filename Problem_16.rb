5fed4eac899560fe9bea68339b621c9abf0a1822
Zuha <zuhaarana@gmail.com>
2023-08-15 11:23:57 +0500

    Problem 16

---
+++ Problem_16.rb
@@ -0,0 +1,9 @@
+# Check if all elements in an array are Fixnum
+
+# Problem Statement:
+# Given an array, return true if all the elements are Fixnums.
+# You need to write array_of_fixnums? method to accomplish this task.
+
+# Example:
+# Given [1,2,3], the method should return true
+
