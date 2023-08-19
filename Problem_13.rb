4a0230c4e11ca3edc11688cd5f132a1717d26c12
Zuha <zuhaarana@gmail.com>
2023-08-19 12:14:58 +0500

    Problem 13

--- Problem_13.rb
+++ Problem_13.rb
@@ -6,6 +6,8 @@
 # Measure and return the time taken to execute that code.
 # You may use Time.now to get the current time.
 
+# Solution:
+
 def exec_time(proc)
   start_time = Time.now
   proc.call
